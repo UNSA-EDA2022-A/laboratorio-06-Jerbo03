d35bdcf102c261cb5cf8420e47683b96cf4a264e
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-07-13 08:16:29 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -21,7 +21,13 @@ public class Exercise1 {
 
 
     public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
-
-        return false;
+        if (a1.isempty() || a1.isempty()) return a1.isempty() && a1.isempty(); // Si alguna de las raices es null, ambas deben serlo para tener un bst similar
+        // Si ninguna raiz es null
+        BST<T> a1L = a1; a1L.root = a1L.left;   // Crear el sub arbol 1 izquierdo
+        BST<T> a1R = a1; a1R.root = a1R.right;  // Crear el sub arbol 1 derecho
+        BST<T> a2L = a2; a2L.root = a2L.left;   // Crear el sub arbol 2 izquierdo
+        BST<T> a2R = a2; a2R.root = a2R.right;  // Crear el sub arbol 2 derecho
+        return bstSimilares(a1L, a2L) && bstSimilares(a1R, a2R); // Y comprobar que los subarboles tambien sean similares
     }
+    
 }
