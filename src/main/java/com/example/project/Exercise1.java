86a277338742f837659f4cb0fe47ba8217ebc607
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-07-13 08:49:21 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -19,15 +19,15 @@ public class Exercise1 {
         
     }
 
-
+    
     public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
-        if (a1.isEmpty() || a2.isEmpty()) return a1.isEmpty() && a2.isEmpty(); // Si alguna de las raices es null, ambas deben serlo para tener un bst similar
+        return bstSimilares(a1.root, a2.root);
+    }
+    
+    public <T extends Comparable<T>> boolean bstSimilares(Node<T> a1, Node<T> a2){
+        if (a1 == null || a2 == null) return a1 == null && a2 == null; // Si alguna de las raices es null, ambas deben serlo para tener un bst similar
         // Si ninguna raiz es null
-        BST<T> a1L = a1; a1L.root = a1L.root.left;   // Crear el sub arbol 1 izquierdo
-        BST<T> a1R = a1; a1R.root = a1R.root.right;  // Crear el sub arbol 1 derecho
-        BST<T> a2L = a2; a2L.root = a2L.root.left;   // Crear el sub arbol 2 izquierdo
-        BST<T> a2R = a2; a2R.root = a2R.root.right;  // Crear el sub arbol 2 derecho
-        return bstSimilares(a1L, a2L) && bstSimilares(a1R, a2R); // Y comprobar que los subarboles tambien sean similares
+        return bstSimilares(a1.left, a2.left) && bstSimilares(a1.left, a2.left); // Comprobar que los subarboles tambien sean similares
     }
     
 }
