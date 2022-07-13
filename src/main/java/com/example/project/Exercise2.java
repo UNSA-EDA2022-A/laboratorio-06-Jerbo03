8bfea06fdeff53063d90f6b6b434a37b1fc676bb
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-07-13 09:38:24 -0500

    Comentarios

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -24,7 +24,7 @@ public class Exercise2 {
     }
     
     public <T extends Comparable<T>> boolean bstIguales(Node<T> a1, Node<T> a2){
-        if (a1 == null || a2 == null) return (a1 == null && a2 == null);
-        return (a1.data.compareTo(a2.data) == 0) && bstIguales(a1.left, a2.left) && bstIguales(a1.right, a2.right);
+        if (a1 == null || a2 == null) return (a1 == null && a2 == null); // Si son nulos, no se pueden verificar sus hijos (no tienen)
+        return (a1.data.compareTo(a2.data) == 0) && bstIguales(a1.left, a2.left) && bstIguales(a1.right, a2.right); // Comparar sus valores y el de sus hijos
     }
 }
